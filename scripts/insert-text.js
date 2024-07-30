2faeb9f20359e851c98dcd622b433fb1157f5a7f
Gabriel Woodger <gabriel@woodge.co.uk>
2024-07-30 13:28:10 +0100

    GW - Added option to upload script to create Insert Text script step rather than Insert Record

---
+++ scripts/insert-text.js
@@ -0,0 +1,24 @@
+const config = require("../widget.config");
+
+const { widgetName, insertTextFileName, insertTextVariableName, insertTextShowConfirmation, server } = config;
+
+console.log(widgetName, insertTextFileName, '$'+insertTextVariableName,);
+const open = require("open");
+const path = require("path");
+
+const insertTextFilePath = path.join(__dirname, "../", insertTextFileName + '.fmp12');
+
+const fileUrl = `fmp://${server}/${insertTextFile}?script=setText&param=`;
+
+const thePath = path.join(__dirname, "../", "dist", "index.html");
+const params = {
+    variableName: insertTextVariableName,
+    thePath,
+    showConfirmation: insertTextShowConfirmation
+};
+const url = fileUrl + encodeURIComponent(JSON.stringify(params));
+
+// Delay the call, so the file has time to open
+setTimeout(function() {
+    open(url);
+}, 500);
---
+++ widget.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  widgetName: "jsDev",
+  server: "$",
+  method: "record",
+  // method: "insertText",
+  file: "jsDev",
+  uploadScript: "UploadToHTML",
+  reloadScript: "Load Widget",
+  startDevScript: "Set to Dev Mode",
+  insertTextFileName: "InsertTextHelper",
+  insertTextVariableName: "widgetHTML",
+  insertTextShowConfirmation: false,
+};
